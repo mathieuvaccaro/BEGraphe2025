b0eb6a90b89d19a26ff5e5b6981081a7eb00e5e2
Luka-hash-go <bore@insa-toulouse.fr>
2025-05-20 09:44:21 +0200

    up dijikstra

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,12 +1,16 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import org.insa.graphs.algorithm.AbstractSolution;
+import org.insa.graphs.algorithm.AbstractSolution.Status;
 import org.insa.graphs.algorithm.utils.BinaryHeap;
 import org.insa.graphs.model.Arc;
 import org.insa.graphs.model.Graph;
 import org.insa.graphs.model.Node;
+import org.insa.graphs.model.Path;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
@@ -36,33 +40,62 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         for (int i = 0; i < nbNodes; i++) {
             Label label = new Label(graph.getNodes().get(i), false, Double.POSITIVE_INFINITY, null);
             arrayLabel.add(label);
-        }
-        arrayLabel.get(0).setCost(0);
-        tas.insert(arrayLabel.get(0));
-
-        // x = sommet min
-        // y = tous les successeurs de cesoemmt min
-
+        } 
+        arrayLabel.get(data.getOrigin().getId()).setCost(0);
+        tas.insert(arrayLabel.get(data.getOrigin().getId()));
+        Label label_Destination = new Label(data.getDestination(), false, Double.POSITIVE_INFINITY, null);
+        Label sommetSucc = new Label(null, false, 0, null); // Juste une décalaration pour pls tard
         for (int i = 1; i < nbNodes; i++) {
             Label sommetMin = tas.deleteMin();
+
             sommetMin.setMarque(true);
             for (Arc succArc : sommetMin.getSommet_courant().getSuccessors())
             {
-                Label sommetSucc = arrayLabel.get(succArc.getDestination().getId());
+                System.out.println("Successeur " + succArc.getDestination().getId());
+
+                sommetSucc = arrayLabel.get(succArc.getDestination().getId());
                 if(!sommetSucc.getMarque())
                 {
-                    sommetSucc.setCost(Double.min(sommetSucc.getCost(), sommetMin.getCost()));
-                    if(sommetMin.getCost() < sommetSucc.getCost()) // Mis a jour <=> sommetMin.getCost() < sommetSucc.getCost()
+                    // On va améliorer le sommet
+                    if(sommetMin.getCost()+succArc.getLength() < sommetSucc.getCost())
                     {
+                        if(sommetSucc.getCost() < Double.POSITIVE_INFINITY)
+                        {
+                            tas.remove(sommetSucc);
+                        }
+                        sommetSucc.setCost(Double.min(sommetSucc.getCost(), sommetMin.getCost()+succArc.getLength()));
                         tas.insert(sommetSucc);
-                        sommetMin = sommetSucc.getPere();
+                        sommetSucc.setPere(sommetMin);
+
                     }
                 }
             }
+            System.out.println("----------------");
         }
 
+        List<Arc> arcsToReturn = new ArrayList<Arc>();
+
+        if(!label_Destination.getMarque())
+        {
+            return new ShortestPathSolution(data, Status.INFEASIBLE);
+        }
+        else
+        {
+            // Il nous faut le dernier label. Par chance c'est SommerSuc
+            while(sommetSucc.getPere() != null)
+             {
+                Arc arc = null;
+                arc = new ArcForward(sommetSucc.getPere().getSommet_courant(), sommetSucc, sommetSucc.getPere().getCost(), )
+                arcsToReturn.add(sommetSucc.get);
+             }
+        }
+
+
+        Path final_path = new Path(graph, arcsToReturn);
+        solution = new ShortestPathSolution(data, Status.OPTIMAL,final_path);
         // when the algorithm terminates, return the solution that has been found
         return solution;
     }
+    
 
 }
